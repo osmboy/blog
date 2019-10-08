3274a7f67bce2c4fd791cca020b7ffdf7317c9fb
osmboy <lei.lei@ostorage.com.cn>
2019-10-08 11:43:01 +0800

    modify some blog style and add one algorithm test

---
+++ js/baidu-tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?a1c96c23ca24491f47f6825174f9b530";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
